898d434f8f98d2c526fc6b2c2e513a7995a29583
dvynshgpt1 <CodewithDivyanshu@gmail.com>
2021-07-31 17:30:14 +0530

    responsive website

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-var audio = document.createElement('AUDIO');
-document.addEventListener('load', () => {
-  audio.setAttribute('src','sound.mp3');
-  audio.loop = true;
-  audio.canPlayType()
-  audio.autoplay()
-  audio.play();
-  document.body.appendChild(audio);
-});
-
- 
-
 
 
 
